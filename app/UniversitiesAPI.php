1dc74cde0aefaf8a02146572234a8948ca4cfe7c
dagnisiljins <dagnis.iljins@gmail.com>
2023-10-19 17:53:54 +0300

    Papildinājumi

--- app/UniversitiesAPI.php
+++ app/UniversitiesAPI.php
@@ -3,29 +3,40 @@
 declare(strict_types=1);
 
 namespace App;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 
 class UniversitiesAPI
 {
     private string $api;
-
+    private Client $httpClient;
 
     public function __construct(string $api)
     {
         $this->api = $api;
+        $this->httpClient = new Client();
 
     }
 
 
     public function search(string $country): ?UniversityCollection {
         $url = $this->api . urlencode($country);
-        $response = file_get_contents($url);
 
-        if ($response === false) {
+        try {
+            $response = $this->httpClient->get($url);
+        } catch (GuzzleException $e) {
+            echo "Failed to fetch data from the API: " . $e->getMessage() . "\n";
+            return null;
+        }
+
+        $body = $response->getBody()->__toString();
+        $data = json_decode($body, true);
+
+        if ($data === false) {
             echo "Failed to fetch data from the API.\n";
             return null;
         }
 
-        $data = json_decode($response);
 
         if ($data === null) {
             echo "Failed to decode JSON data.\n";
@@ -34,11 +45,11 @@ class UniversitiesAPI
 
         $collection = new UniversityCollection();
 
-        foreach ($data as $index => $university) { //$index var ņemt ārā
+        foreach ($data as $university) {
             $collection->add(new University(
-                $university->name,
-                $university->country,
-                (array)$university->web_pages
+                $university['name'],
+                $university['country'],
+                $university['web_pages']
             ));
         }
 
